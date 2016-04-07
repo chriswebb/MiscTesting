4cf58b94d2fce253fbc3e88ca5926d43d7f0d7f7
Chris Webb <chriswebb@users.noreply.github.com>
2016-04-07 16:48:06 -0500

    Update AssemblyInfo.cs

--- AppHostTest/Properties/AssemblyInfo.cs
+++ AppHostTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CoreDumpAppTest")]
+[assembly: AssemblyTitle("AppHostTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CoreDumpAppTest")]
+[assembly: AssemblyCompany("Chris Webb")]
+[assembly: AssemblyProduct("AppHostTest")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
